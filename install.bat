05acce1c69d752df66c9f0b51fe03f1e9c60f9bf
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-11-18 17:39:45 +0330

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,7 @@
+
+echo off
+color 1
+cls
+:a
+npm i @discordjs/opus array-move cli-color discord-buttons cpu-stat dotenv erit-ytdl express  i18n lyrics-finder discord.js@^12 moment quick.db simple-youtube-api soundcloud-downloader string-progressbar ytdl-core ytdl-core-discord
+goto a
