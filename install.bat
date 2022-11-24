77da59b641fcebd0b7d55483d8c08739756cc4c1
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-11-24 12:08:32 +0330

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@ echo off
 color 1
 cls
 :a
-npm install @discordjs/opus array-move cli-color discord-buttons cpu-stat dotenv erit-ytdl express  i18n lyrics-finder discord.js@^12 moment quick.db simple-youtube-api soundcloud-downloader string-progressbar ytdl-core ytdl-core-discord
+npm install @discordjs/opus array-move cli-color discord-buttons cpu-stat dotenv fs erit-ytdl express  i18n lyrics-finder discord.js@^12 moment quick.db simple-youtube-api soundcloud-downloader string-progressbar ytdl-core ytdl-core-discord
 goto a
